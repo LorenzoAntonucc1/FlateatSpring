c27757fae158aa5775e2ab5b40b3b5af39312de2
niko Ricci <MY_NAssME@example.com>
2024-02-27 14:55:21 +0100

    v5

---
+++ flateat/src/main/java/dto/User/UserDtoBase.java
@@ -0,0 +1,18 @@
+package dto.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+
+public class UserDtoBase 
+{
+
+    private String email;
+    private String password;
+    
+}
