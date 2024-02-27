95cc47124ef2b8530f4bcf40e4c6efaedfba3bb2
Lorenzo Antonucci <lorenzo.antonucci98@gmail.com>
2024-02-27 10:52:16 +0100

    PrimoCommit Spring

---
+++ flateat/src/main/java/com/generation/flateat/FlateatApplication.java
@@ -0,0 +1,13 @@
+package com.generation.flateat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlateatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlateatApplication.class, args);
+	}
+
+}
---
+++ flateat/src/test/java/com/generation/flateat/FlateatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.flateat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlateatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
