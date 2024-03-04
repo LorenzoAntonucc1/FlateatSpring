991229bc426d4e2affe0b071f7455d26237c5754
Lorenzo Antonucci <lorenzo.antonucci98@gmail.com>
2024-03-04 11:01:56 +0100

    PostDelivery

--- flateat/src/main/java/com/generation/flateat/controllers/DeliveryController.java
+++ flateat/src/main/java/com/generation/flateat/controllers/DeliveryController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.generation.flateat.model.dto.delivery.DeliveryDtoR;
 import com.generation.flateat.model.dto.delivery.DeliveryDtoWFull;
 import com.generation.flateat.model.dto.delivery.DeliveryDtoWNoTotalPrice;
 import com.generation.flateat.model.dtoservices.DeliveryConverter;
+import com.generation.flateat.model.entities.Delivery;
 import com.generation.flateat.model.repositories.DeliveryRepository;
 
 @RestController
@@ -40,6 +44,13 @@ public class DeliveryController
     public DeliveryDtoWFull getDeliveryByRestaurantId(@PathVariable Integer deliveryId) 
     {
         return conv.DeliveryToDtoWFull(repo.findById(deliveryId).get());
+    }
+
+    @PostMapping("/delivery")       
+    public Delivery postDelivery(@RequestBody DeliveryDtoR dto)
+    {
+        Delivery delivery = conv.dtoRToDelivery(dto);
+        return delivery;
     }
 }
 
